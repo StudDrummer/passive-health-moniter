cdaa2950fa934233988fdfb7704faf36b80943dd
StudDrummer <huskyrushil10@gmail.com>
2026-04-14 15:41:46 -0700

    removing sendgrid stuff for now

--- Server.py
+++ Server.py
@@ -559,13 +559,3 @@ if __name__ == "__main__":
     print("VIGIL sync server starting on port 5001...")
     app.run(host="0.0.0.0", port=5001, debug=False)
 
-sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
-from_email = Email("test@example.com")
-to_email = To("test@example.com")
-subject = "Sending with SendGrid is Fun"
-content = Content("text/plain", "and easy to do anywhere, even with Python")
-mail = Mail(from_email, to_email, subject, content)
-response = sg.client.mail.send.post(request_body=mail.get())
-print(response.status_code)
-print(response.body)
-print(response.headers)
